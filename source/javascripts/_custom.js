0180ea326f5bac721d93a452b5b9065475019f39
dannyvassallo <danielvassallo87@gmail.com>
2015-10-07 20:57:23 -0400

    established game logic

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -5,6 +5,7 @@
 var userScore = 0;
 var enemyScore = 0;
 var theThrow = '';
+var enemyThrow = '';
 
 // ******************** //
 //    Enemy Components  //
@@ -26,7 +27,7 @@ function userThrow(userInputClass){
 	$(userInputClass).click(function(e){
 		e.preventDefault();
 		theThrow = $(this).attr("class");
-		console.log(theThrow);
+		console.log("USER THREW: "+theThrow);
 	});
 }
 
@@ -37,11 +38,37 @@ function theBattle(theControls){
 	$(theControls).click(function(){
 		// Randomize An Index Number From "EnemyThrowStrings"
 		var randomIndex = Math.floor(Math.random()*enemyThrowStrings.length);
-		var enemyThrow = enemyThrowStrings[randomIndex];
-		console.log(enemyThrow);
+		// Enemy's Throw
+		enemyThrow = enemyThrowStrings[randomIndex];
+		console.log("ENEMY THREW: "+enemyThrow);
+		// Tie
 		if(theThrow == enemyThrow){
-			console.log('tied');
+			console.log('tied TIE GAME');
 		}
+		// User Throws Rock
+		else if(theThrow == 'rock'){
+			if(enemyThrow == 'scissors'){
+				console.log('rock beats scissors USER WINS');
+			} else if(enemyThrow == 'paper'){
+				console.log('paper covers rock USER LOSES');
+			}
+		}
+		// User Throws Scissors
+		else if(theThrow == 'scissors'){
+			if(enemyThrow == 'rock'){
+				console.log('rock beats scissors USER LOSES');
+			} else if(enemyThrow == 'paper'){
+				console.log('scissors cut paper USER WINS');
+			}
+		}
+		// User Throws Paper
+		else if(theThrow == 'paper'){
+			if(enemyThrow == 'rock'){
+				console.log('paper covers rock USER WINS');
+			} else if(enemyThrow == 'scissors'){
+				console.log('scissors cut paper USER LOSES');
+			}
+		}		
 	});
 }
 
