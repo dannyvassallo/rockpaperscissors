1e9aabb33baec7ecdc631d04ff35ebad0eddf78e
dannyvassallo <danielvassallo87@gmail.com>
2015-10-07 20:22:41 -0400

    established basic user input

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,19 +1,50 @@
-// Document Ready
+
+// ******************** //
+//    Player Scores     //
+// ******************** //
+var userScore = 0;
+var enemyScore = 0;
+
+
+// ******************** //
+//    Enemy Components  //
+// ******************** //
+
+
+// Enemy Throw String Array
+var enemyThrowStrings = [
+    'rock',
+    'paper',
+    'scissors'
+];
+
+// Randomize An Index Number From "EnemyThrowStrings"
+var randomIndex = Math.floor(Math.random()*enemyThrowStrings.length);
+console.log(enemyThrowStrings[randomIndex]);
+
+
+// ******************** //
+//    User Components   //
+// ******************** //
+function userThrow(userInputClass){
+	$(userInputClass).click(function(e){
+		e.preventDefault();
+		var theThrow = $(this).attr("class");
+		console.log(theThrow);
+	});
+}
+
+// ******************** //
+// ******************** //
+//   Document Ready     //
+// ******************** //
+// ******************** //
+
 $(function(){
-	// ******************** //
-	//    Enemy Components  //
-	// ******************** //
-
-
-	// Enemy Throw String Array
-	var enemyThrowStrings = [
-	    'rock',
-	    'paper',
-	    'scissors'
-	];
-
-	// Randomize An Index Number From "EnemyThrowStrings"
-	var randomIndex = Math.floor(Math.random()*enemyThrowStrings.length);
-	console.log(enemyThrowStrings[randomIndex]);
-
+// ******************** //
+//       User Input     //
+// ******************** //
+userThrow('.rock');
+userThrow('.paper');
+userThrow('.scissors');
 });
