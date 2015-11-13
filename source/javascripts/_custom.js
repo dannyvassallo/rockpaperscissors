170fda673ec693c5e25d0107318c89d2abf755a1
dannyvassallo <danielvassallo87@gmail.com>
2015-11-12 23:51:00 -0500

    scores and rounds updating on screen

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,17 +1,16 @@
 
 // ******************** //
-//    Player Scores     //
+//       Scores         //
 // ******************** //
 var userScore = 0;
 var enemyScore = 0;
+var roundCounter = 0;
 var theThrow = '';
 var enemyThrow = '';
 
 // ******************** //
 //    Enemy Components  //
 // ******************** //
-
-
 // Enemy Throw String Array
 var enemyThrowStrings = [
     'rock',
@@ -23,6 +22,9 @@ var enemyThrowStrings = [
 // ******************** //
 //    User Components   //
 // ******************** //
+// This function takes the class from the user's
+// clicked element and turns it into a string
+// that the "Battle" function can understand
 function userThrow(userInputClass){
 	$(userInputClass).click(function(e){
 		e.preventDefault();
@@ -36,15 +38,29 @@ function userThrow(userInputClass){
 // ******************** //
 //       User Input     //
 // ******************** //
+// This registers the users throw on click.
+// It takes the users input on click and calls the previous
+// user throw function
 userThrow('.rock');
 userThrow('.paper');
 userThrow('.scissors');
-theBattle('#gameboy .control');
 
 
 // ******************** //
 //    Game Logic   //
 // ******************** //
+
+
+// this function updates the user and enemy score on the page
+function updateScores(){
+	$('.user-score .score-value').html(userScore);
+	$('.enemy-score .score-value').html(enemyScore);
+	$('.round-counter .value').html(roundCounter);
+}
+
+
+// This takes a class applied to all user throw inputs
+// and checks the user throw. The enemy's logic is also decided here.
 function theBattle(theControls){
 	$(theControls).click(function(){
 		// Randomize An Index Number From "EnemyThrowStrings"
@@ -55,43 +71,47 @@ function theBattle(theControls){
 		// Tie
 		if(theThrow == enemyThrow){
 			alert('tied TIE GAME');
+			roundCounter+=1;
+			updateScores();
 		}
 		// User Throws Rock
 		else if(theThrow == 'rock'){
 			if(enemyThrow == 'scissors'){
 				alert('rock beats scissors USER WINS');
+				userScore+=1;
 			} else if(enemyThrow == 'paper'){
 				alert('paper covers rock USER LOSES');
+				enemyScore+=1;
 			}
+			roundCounter+=1;
+			updateScores();
 		}
 		// User Throws Scissors
 		else if(theThrow == 'scissors'){
 			if(enemyThrow == 'rock'){
 				alert('rock beats scissors USER LOSES');
+				userScore+=1;
 			} else if(enemyThrow == 'paper'){
 				alert('scissors cut paper USER WINS');
+				enemyScore+=1;
 			}
+			roundCounter+=1;
+			updateScores();
 		}
 		// User Throws Paper
 		else if(theThrow == 'paper'){
 			if(enemyThrow == 'rock'){
 				alert('paper covers rock USER WINS');
+				userScore+=1;
 			} else if(enemyThrow == 'scissors'){
 				alert('scissors cut paper USER LOSES');
+				enemyScore+=1;
 			}
+			roundCounter+=1;
+			updateScores();
 		}		
 	});
 }
 
-
-// ******************** //
-// ******************** //
-//   Document Ready     //
-// ******************** //
-// ******************** //
-
-
-$(function(){
-
-
-});
+// Start the game
+theBattle('.control');
